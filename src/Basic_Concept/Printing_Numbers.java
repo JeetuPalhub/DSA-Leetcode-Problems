9358811087e4fbf0d00cf9c0c7860d1dc832a2d9
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-10-04 21:13:44 +0530

    more question

--- src/Basic_Concept/Printing_Numbers.java
+++ src/Basic_Concept/Printing_Numbers.java
@@ -1,6 +1,6 @@
 package Basic_Concept;
 
-public class Printing_Numbers {
+public class  Printing_Numbers {
     public static void main(String[] args) {
         System.out.println(10+3);
         System.out.println("10+3");
---
+++ src/Basic_Concept/VolumeOfSphere.java
@@ -0,0 +1,9 @@
+package Basic_Concept;
+
+public class VolumeOfSphere {
+   public static void main(String[] args) {
+        double radius = 7.7;
+        double volume = 4.0/3.0 * 3.14 * radius * radius * radius;
+       System.out.println("Volume: " + volume);
+    }
+}
